acd2405abd42f7da9777540da988d2ee17306ffe
jim-nnamdi <jimsamuel50@gmail.com>
2023-03-08 11:09:27 +0100

    add cmd for creating new file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -36,6 +37,32 @@ func main() {
 func execInput(kinput string) error {
 	rmline := strings.TrimSuffix(kinput, "\n")
 	args := strings.Split(rmline, " ")
+
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			fmt.Fprintf(os.Stderr, "%s", errors.New("path required"))
+			return errors.New("path required")
+		}
+		return os.Chdir(args[1])
+	case "touch":
+		if len(args) < 2 {
+			fmt.Fprintf(os.Stderr, "%s", errors.New("no file specified"))
+			isfile := strings.Contains(args[1], ".")
+			if !isfile {
+				fmt.Fprintf(os.Stderr, "%s", errors.New("specify a valid extension"))
+				return errors.New("specify a valid file extension")
+			}
+			newfile, e := os.Create(args[1])
+			if e != nil {
+				fmt.Fprintf(os.Stderr, "%s", errors.New("cannot create file"))
+				return e
+			}
+			return newfile.Chmod(0777)
+		}
+	case "exit":
+		os.Exit(0)
+	}
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
